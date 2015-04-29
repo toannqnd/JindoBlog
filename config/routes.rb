eaa8902ca9c5bc804837fc4d59704417b655087f
Nguyen Toan <toannq@nal.vn>
2015-04-29 12:07:30 +0700

    bat dau video 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root                'static_pages#home'
   get 'contact'   =>  'static_pages#contact'
   get 'about'     =>  'static_pages#about'
   get 'help'      =>  'static_pages#help'
   get 'signup'    =>  'users#new'
+
+  resource :users
 end
