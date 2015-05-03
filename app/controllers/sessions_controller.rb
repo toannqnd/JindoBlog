d0f8c1837b425abb76002a72501777de7d3bba6a
Nguyen Toan <toannq@nal.vn>
2015-05-03 14:39:24 +0700

    Xong video 08

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # log the user in and redirect to the user's show page
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to users_path user
     else
       # Create an error message
@@ -16,6 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    log_out
+    redirect_to root_url
   end
 end
