ffb203273407690dc2b247d658f9a5aeb4a23b80
Nguyen Toan <toannq@nal.vn>
2015-05-05 23:10:44 +0700

    Xong video 09

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # Sử dụng bộ lọc để kiểm tra trạng thái đăng nhập
   # Nếu người dùng truy cập vô edit, update thì chạy vào 'logged_in_user' để kiểm tra
-  before_action :logged_in_user,  only:  [:index, :edit, :update]
+  before_action :logged_in_user,  only:  [:index, :edit, :update, :destroy]
   before_action :correct_user,    only:  [:edit, :update]
   before_action :admin_user,      only:  :destroy
 
@@ -40,6 +40,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
